ec00251cf2530efe876e8dfd92497e1b923ad071
IDK <114621472+yamada-sexta@users.noreply.github.com>
2026-03-14 18:19:05 -0500

    Update pages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig, loadEnv } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
-export default defineConfig({
-  plugins: [solidPlugin()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
+  const basePath = env.VITE_BASE_PATH || "/";
+
+  return {
+    base: basePath,
+    plugins: [solidPlugin()],
+    server: {
+      port: 3000,
+    },
+    build: {
+      target: "esnext",
+    },
+  };
 });
